8dc57fa9f414665944c122fc7b48faed06d6316a
01anto <01anto@live.ie>
2022-10-15 21:24:56 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 import gspread
 
 from google.oauth2.service_account import Credentials
@@ -27,7 +26,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
@@ -130,7 +129,5 @@ def main():
     update_worksheet(stock_data, "stock")
 
 
-
 print("Welcome to Love Sandwiches Data Automation")
 main()
-
